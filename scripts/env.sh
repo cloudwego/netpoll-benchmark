7aff6c15b84b4b7de54e91fc532715d7b14dbeb1
wangzhuowei <wangzhuowei@bytedance.com>
2022-02-07 14:43:19 +0800

    fix: bind numa nodes

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# cpu binding
 nprocs=$(getconf _NPROCESSORS_ONLN)
 if [ $nprocs -lt 4 ]; then
   echo "Your environment should have at least 4 processors"
@@ -7,10 +8,17 @@ if [ $nprocs -lt 4 ]; then
 elif [ $nprocs -gt 20 ]; then
   nprocs=20
 fi
-
-scpu=$((nprocs > 16 ? 3 : nprocs / 4 - 1)) # max is 3(4 cpus)
-taskset_server="taskset -c 0-$scpu"
-taskset_client="taskset -c $((scpu + 1))-$((nprocs - 1))"
+scpu=$((nprocs > 16 ? 4 : nprocs / 4)) # max is 4 cpus
+ccpu=$((nprocs-scpu))
+scpu_cmd="taskset -c 0-$((scpu-1))"
+ccpu_cmd="taskset -c ${scpu}-$((ccpu-1))"
+if [ -x "$(command -v numactl)" ]; then
+  # use numa affinity
+  node0=$(numactl -H | grep "node 0" | head -n 1 | cut -f "4-$((3+scpu))" -d ' ' --output-delimiter ',')
+  node1=$(numactl -H | grep "node 1" | head -n 1 | cut -f "4-$((3+ccpu))" -d ' ' --output-delimiter ',')
+  scpu_cmd="numactl -C ${node0} -m 0"
+  ccpu_cmd="numactl -C ${node1} -m 1"
+fi
 
 # benchmark, args1=server|client
 function benchmark() {
@@ -29,17 +37,17 @@ function benchmark() {
         if [[ "$1" == "client" ]]; then
           svr="net_reciever"
         fi
-        nohup $taskset_server ./output/bin/${svr} -addr="$addr" -mode=$mode >>output/log/nohup.log 2>&1 &
+        nohup $scpu_cmd ./output/bin/${svr} -addr="$addr" -mode=$mode >>output/log/nohup.log 2>&1 &
         sleep 1
-        echo "server $svr running with $taskset_server"
+        echo "server $svr running with $scpu_cmd"
 
         # run client
         cli=${rp}_bencher
         if [[ "$1" == "server" ]]; then
           cli="net_bencher"
         fi
-        echo "client $cli running with $taskset_client"
-        $taskset_client ./output/bin/${cli} -name="$rp" -addr="$addr" -mode=$mode -b=$b -c=$c -n=$n
+        echo "client $cli running with $ccpu_cmd"
+        $ccpu_cmd ./output/bin/${cli} -name="$rp" -addr="$addr" -mode=$mode -b=$b -c=$c -n=$n
 
         # stop server
         pid=$(ps -ef | grep $svr | grep -v grep | awk '{print $2}')
